99c504420e9a5a57a8b6f158e0fec7697de4349b
Nicholas Nascimento <nicholas.an99@gmail.com>
2024-06-04 19:28:37 -0300

    feat: Adicionando chamadas de Login e Registro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,19 @@ class SessionsController < ApplicationController
       render json: { error: "Invalid username or password" }, status: :unauthorized
     end
   end
+
+  def register
+    user = User.new(user_params)
+    if user.save
+      render json: { id: user.id, username: user.username, admin: user.admin }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
