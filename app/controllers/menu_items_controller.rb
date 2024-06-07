b81f5dbf39228742c59ecce93bdb14817fdefc15
Nicholas Nascimento <nicholas.an99@gmail.com>
2024-06-06 22:15:39 -0300

    feat: Adicionando criacao de items no Menu para user admin

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -4,6 +4,15 @@ class MenuItemsController < ApplicationController
     render json: @menu_items
   end
 
+  def create
+    @menu_item = MenuItem.new(menu_item_params)
+    if @menu_item.save
+      render json: @menu_item, status: :created
+    else
+      render json: @menu_item.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     menu_item = MenuItem.find_by(id: params[:id])
     if menu_item&.destroy
@@ -11,5 +20,11 @@ class MenuItemsController < ApplicationController
     else
       render json: { error: 'Item not found' }, status: :not_found
     end
+  end
+
+  private
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :description, :price, :item_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   post 'register', to: 'sessions#register'
 
-  resources :menu_items, only: [:index, :destroy]
+  resources :menu_items, only: [:index, :destroy, :create]
 end
